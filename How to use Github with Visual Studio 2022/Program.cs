809b449f4cc43919cfbad65ed9c043bf688789a9
Abdellateef Eid <abdellateef35@gmail.com>
2024-09-26 21:35:31 +0300

    Add project files.

---
+++ How to use Github with Visual Studio 2022/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace How_to_use_Github_with_Visual_Studio_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
